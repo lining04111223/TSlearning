2e18dc2e65bc7fcccbbd5d0781663ec9f2d87b7f
Ning Li <lining04111223@gmail.com>
2023-01-03 21:01:21 -0800

    learn ts

---
+++ 01_adding-types.js
@@ -0,0 +1,9 @@
+var user = "Alice";
+user = "Bob";
+user = 42;
+user = true;
+var users = [];
+//users.push("Alice");
+//users.push(1);
+//let aaa =11;
+var result = users.pop();
---
+++ 01_adding-types.ts
@@ -0,0 +1,13 @@
+let user: string | number | boolean = "Alice";
+
+user = "Bob";
+user = 42;
+user = true;
+
+const users: string[] = [];
+//users.push("Alice");
+
+//users.push(1);
+
+//let aaa =11;
+const result = users.pop();
